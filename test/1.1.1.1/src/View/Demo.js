b9927386127a7035c2725cdfe9237c5847d5ac6a
yicheny <1463020554@qq.com>
2019-08-28 16:30:37 +0800

    脚本测试中3

---
+++ test/1.1.1.1/src/View/Demo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {data} from "../Data/data";
+
+function Demo(props) {
+    console.log(data);
+    return (
+        <div>
+            treeTable
+        </div>
+    );
+}
+
+export default Demo;
