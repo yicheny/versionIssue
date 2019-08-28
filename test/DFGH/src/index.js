09fb5378ab3fc39104d1425974415a74ac0ac091
yicheny <1463020554@qq.com>
2019-08-28 11:30:16 +0800

    脚本测试中3

---
+++ test/DFGH/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Demo from './View/Demo';
+import './index.less'
+
+ReactDOM.render(
+    <Demo/>,
+    document.getElementById('root')
+);
+
+
